f45e0279bbfd678878c47044690436a56d924cb8
Maaz Uddin <maaz.uddin@berkeley.edu>
2016-03-18 14:08:16 -0400

    Done. Sorry I submitted late, I was on a plane last night, and didn't realize till this morning that I didn't ommit and push my code.

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -14,19 +14,21 @@ class GameViewController: UIViewController {
     @IBOutlet var hangmanImage: UIImageView!
     @IBOutlet weak var guessButton: UIButton!
     @IBOutlet weak var displayedPhrase: UILabel!
+    @IBOutlet weak var wrongLettersLabel: UILabel!
     
     let hangmanImagesArray: [String] = ["hangWizard1", "hangWizard2", "hangWizard3", "hangWizard4", "hangWizard5", "hangWizard6", "hangWizard7"]
     var wronglyGuessedLetters: [String] = []
+    var correctlyGuessedLetters: [String] = []
     var stringOfCorrectLettersAndUnderscores: String = ""
     var phraseThatPlayerMustGuessLettersOf: String = ""
     var numberOfWrongGuesses = 0
     var phraseLength = 0
+    var gameState = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-//        self.view.backgroundColor = UIImageView(image: "matrixBackground.jpg")
         let hangmanPhrases = HangmanPhrases()
         let phrase = hangmanPhrases.getRandomPhrase()
         phraseThatPlayerMustGuessLettersOf = phrase
@@ -52,14 +54,7 @@ class GameViewController: UIViewController {
         return true
     }
     
-    @IBAction func whenGuessIsClicked(sender: AnyObject) {
-        if numberOfWrongGuesses != 6 {
-            numberOfWrongGuesses += 1
-            hangmanImage.image = UIImage(named: hangmanImagesArray[numberOfWrongGuesses])
-        }
-        updateUILabel()
-    }
-    
+    // Initializes the game with "-" for every corresponding letter
     func initializeUILabel() {
         for char in phraseThatPlayerMustGuessLettersOf.characters {
             if char == " " {
@@ -68,21 +63,113 @@ class GameViewController: UIViewController {
                 stringOfCorrectLettersAndUnderscores += "- "
             }
         }
+        displayedPhrase.text = stringOfCorrectLettersAndUnderscores
     }
     
-    func updateUILabel() {
-        for char in phraseThatPlayerMustGuessLettersOf.characters {
-            if String(char) == guessedLetterFromTextField.text!.lowercaseString {
-                stringOfCorrectLettersAndUnderscores += guessedLetterFromTextField.text!
-            }
-            if char == " " {
-                stringOfCorrectLettersAndUnderscores += "  "
+    // When Guess is clicked, either correctly change the letter in the text field
+    // or will update the hangman accordingly
+    @IBAction func whenGuessIsClicked(sender: AnyObject) {
+        if gameState {
+            if guessedLetterFromTextField.text!.uppercaseString.characters.count != 1 {
+                let invalidInputAlert = UIAlertController(title: "Invalid Input", message: "You must input 1 character", preferredStyle: UIAlertControllerStyle.Alert)
+                invalidInputAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(invalidInputAlert, animated: true, completion: nil)
+                
+            } else if correctlyGuessedLetters.contains(guessedLetterFromTextField.text!.uppercaseString) {
+                let correctlyGuessedAlert = UIAlertController(title: "Please Try Again", message: "You already tried guessing that letter", preferredStyle: UIAlertControllerStyle.Alert)
+                correctlyGuessedAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(correctlyGuessedAlert, animated: true, completion: nil)
+                guessedLetterFromTextField.text = ""
+                
+            } else if isGuessedLetterInPhrase() {
+                updateUILabel()
+                if stringOfCorrectLettersAndUnderscores == phraseThatPlayerMustGuessLettersOf {
+                    gameHasEnded()
+                }
             } else {
-                stringOfCorrectLettersAndUnderscores += "- "
+                if wronglyGuessedLetters.contains(guessedLetterFromTextField.text!.uppercaseString) {
+                    let wronglyGuessedAlert = UIAlertController(title: "Please Try Again", message: "You already tried guessing that letter", preferredStyle: UIAlertControllerStyle.Alert)
+                    wronglyGuessedAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(wronglyGuessedAlert, animated: true, completion: nil)
+                    guessedLetterFromTextField.text = ""
+                    
+                } else {
+                    wrongLettersLabel.text! += guessedLetterFromTextField.text!.uppercaseString
+                    wrongLettersLabel.text! += " "
+                    wronglyGuessedLetters.append(guessedLetterFromTextField.text!.uppercaseString)
+                    numberOfWrongGuesses += 1
+                    hangmanImage.image = UIImage(named: hangmanImagesArray[numberOfWrongGuesses])
+                    guessedLetterFromTextField.text = ""
+                    if numberOfWrongGuesses == 6 {
+                        gameHasEnded()
+                    }
+                }
             }
         }
+    }
+    
+    // Helper function to see if the guessed character is in the phrase
+    func isGuessedLetterInPhrase() -> Bool {
+        return phraseThatPlayerMustGuessLettersOf.containsString(guessedLetterFromTextField.text!.uppercaseString)
+    }
+    
+    // Update the UI Label with the guessed character
+    func updateUILabel() {
+        var temp = ""
+        for char in phraseThatPlayerMustGuessLettersOf.characters {
+            if correctlyGuessedLetters.contains(String(char)) {
+                temp += String(char).uppercaseString
+            } else if String(char).uppercaseString == guessedLetterFromTextField.text!.uppercaseString {
+                temp += guessedLetterFromTextField.text!.uppercaseString
+                correctlyGuessedLetters.append(String(char).uppercaseString)
+            } else if char == " " {
+                temp += " "
+            } else {
+                temp += "-"
+            }
+        }
+        stringOfCorrectLettersAndUnderscores = temp
         displayedPhrase.text = stringOfCorrectLettersAndUnderscores
-        print(guessedLetterFromTextField.text)
+        guessedLetterFromTextField.text = ""
+    }
+    
+    // Set the gameState to false, disallowing any further moves to be made
+    func gameHasEnded() {
+        gameState = false
+        if numberOfWrongGuesses == 6 {
+            let loseAlert = UIAlertController(title: "SORRY :(", message: "You Ran Out Of Lives", preferredStyle: UIAlertControllerStyle.Alert)
+            loseAlert.addAction(UIAlertAction(title: "Start Over", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in self.startOver("") }))
+            loseAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            loseAlert.addAction(UIAlertAction(title: "New Game", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in self.newGame() }))
+            self.presentViewController(loseAlert, animated: true, completion: nil)
+
+        } else {
+            let winAlert = UIAlertController(title: "CONGRATULATIONS", message: "You Guessed All The Correct Letters", preferredStyle: UIAlertControllerStyle.Alert)
+            winAlert.addAction(UIAlertAction(title: "Start Over", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in self.startOver("") }))
+            winAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            winAlert.addAction(UIAlertAction(title: "New Game", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in self.newGame() }))
+            self.presentViewController(winAlert, animated: true, completion: nil)
+        }
+    }
+    
+    // Start a new game with a new word
+    func newGame() {
+        gameState = true
+        viewDidLoad()
+        startOver("")
+    }
+    
+    // Start a new game with the same word
+    @IBAction func startOver(sender: AnyObject) {
+        gameState = true
+        numberOfWrongGuesses = 0
+        hangmanImage.image = UIImage(named: hangmanImagesArray[numberOfWrongGuesses])
+        stringOfCorrectLettersAndUnderscores = ""
+        wrongLettersLabel.text! = "Incorrect Guesses: "
+        wronglyGuessedLetters = []
+        correctlyGuessedLetters = []
+        initializeUILabel()
+
     }
 
     /*
